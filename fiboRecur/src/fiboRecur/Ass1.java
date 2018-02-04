7b7f6ea272d75f356b08b97cf2ef8ee1fd8a5378
DierksBentely <35244998+DierksBentely@users.noreply.github.com>
2018-02-04 18:18:27 -0500

    added comments

--- fiboRecur/src/fiboRecur/Ass1.java
+++ fiboRecur/src/fiboRecur/Ass1.java
@@ -1,29 +1,41 @@
+// Graham Pomfret
+// student ID 0595 995
+// this is a program to call a recursive function to display fibbonacci numbers to an nth degree
 package fiboRecur;
 
+// importing utilities for Java
+// scanner to take user input
+// time unit to time how long the program takes
 import java.util.concurrent.TimeUnit;
 import java.util.Scanner;
 
 public class Ass1 
 {
+	// main method
 	public static void main(String[] args)
 	{
+		// initializing variable to store start time
 		long startTime = System.nanoTime();
 		
+		// user input
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("please enter a number to calculate fibbonacci numbers to");
 		int userNum = userInput.nextInt();
 		
+		// printing error message if user's number < 1
 		if (userNum < 0)
 		{
-			userNum = 0;
-			
+			System.out.println("cannot calculate fibbonacci numbers to a negative number");
 		}
-		
-		for (int i = 0; i<= userNum; i++)
+
+		// loop calling recursive function with i
+		for (int i = 0; i<= userNum-1; i++)
 		{
+			// recursive method call function
 			System.out.println(fiboRecur(i));
 		}
 		
+		// initialize vaiable for end time
 		long endTime = System.nanoTime();
 		
 		System.out.println("Time elapsed was: " + (endTime - startTime) + " nanoseconds");
@@ -31,14 +43,17 @@ public class Ass1
 
 	public static int fiboRecur(int fiboNum)
 	{
+		// make 0 = 0, which is the 1st number as a constant
 		if (fiboNum < 1)
 		{
 			return 0;
 		}
+		// make 1 = 1, second constant
 		if (fiboNum == 1 || fiboNum == 2)
 			return 1;
 		
-		return fiboRecur(fiboNum-1) + fiboRecur(fiboNum-2);
+		// method returns new parameters
+		return (fiboRecur(fiboNum-1) + fiboRecur(fiboNum-2));
 	}
 	
 }
